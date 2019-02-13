1cc0b0a783525d2f72b6626b7c15ad700678e55a
Dillan Gibbons <Dillan.G.Gibbons-1@ou.edu>
2019-02-12 22:29:24 -0600

    Wrote AnimalTest.java

--- src/AnimalTest.java
+++ src/AnimalTest.java
@@ -21,7 +21,21 @@ public class AnimalTest
         Assert.assertEquals(0, animal.getHeight(), 0.01);
         Assert.assertEquals(0, animal.getWeight(), 0.01);
     }
-
-    // TODO: test full constructor, getters, and toString
+    public void fullConstructorTest() throws AssertException {
+    	//use full constructor
+    	Animal animal = new Animal("test", "testing", 1.0, 1.0);
+    	
+    	//The color should be "test", name "testing", height and weight 1.0
+    	Assert.assertEquals("test", animal.getColor());
+    	Assert.assertEquals("testing", animal.getName());
+    	Assert.assertEquals(1.0, animal.getHeight(), 0.01);
+    	Assert.assertEquals(1.0, animal.getWeight(), 0.01);
+    }
+    public void toStringTest() throws AssertException {
+    	Animal animal = new Animal("test", "testing", 1.0, 1.0);
+    	String info = animal.toString();
+    	
+    	Assert.assertEquals("testing, a test-colored animal. 1.0 pounds, 1.0 inches\n", info);
+    }
 }
 
